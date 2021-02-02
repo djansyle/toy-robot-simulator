b2345c93d6408d9969c512344ebf5b0fb00b048a
djansyle <djansyledjans@gmail.com>
2021-02-02 22:12:30 +0800

    feat: add reading file input and line

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,46 @@
+/* eslint-disable no-console */
 import { Command } from 'commander';
+import fs from 'fs';
+import path from 'path';
+import readlineSync from 'readline-sync';
+import CommandProcessor from './classes/command-processor';
+import OperationError from './classes/operation-error';
 
 const program = new Command();
-
 program.option('-i, --input <file>', 'Input file');
-
 program.parse(process.argv);
 
-if (program.opts().input) {
-  console.log(program.opts());
+const inputFile = program.opts().input;
+const commandProcessor = new CommandProcessor();
+commandProcessor.on('data', (message) => console.log('Output:', message));
+
+function tryProcess(line: string) {
+  try {
+    commandProcessor.process(line);
+  } catch (e) {
+    if (e instanceof OperationError) {
+      return;
+    }
+
+    throw e;
+  }
+}
+
+if (inputFile) {
+  const lines = fs
+    .readFileSync(path.join(process.cwd(), inputFile))
+    .toString()
+    .split('\n')
+    .map((line) => line.trim());
+
+  lines.forEach((line) => {
+    console.log(line);
+    tryProcess(line);
+  });
+}
+
+// eslint-disable-next-line no-constant-condition
+while (true) {
+  const line = readlineSync.question('');
+  tryProcess(line.trim());
 }
